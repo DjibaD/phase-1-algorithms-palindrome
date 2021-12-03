864500bb024d281a9406249dc02f025415cfa3bc
djibadiawara <djiba.diawara97@gmail.com>
2021-12-03 16:44:29 -0500

    commit tree 858f3c8df4b143066f0a7e1d1c2475dba665d81d

--- index.js
+++ index.js
@@ -1,6 +1,32 @@
 function isPalindrome(word) {
   // Write your algorithm here
+  // iterate from the beginning to the middle
+  for (let i = 0; i < word.length / 2; i++) {
+    // check each letter to the correponding letter from the end
+    const j = word.length - 1 - i;
+    // if any letters don't match, return false
+    if (word[i] !== word[j]) return false;
+  }
+
+  // return true
+  return true;
 }
+
+/* 
+r a c e c a r
+0 1 2 3 4 5 6
+i           j
+r a c e c a r
+0 1 2 3 4 5 6
+  i       j
+r a c e c a r
+0 1 2 3 4 5 6
+    i   j
+r a c e c a r
+0 1 2 3 4 5 6
+      ij
+}
+
 
 /* 
   Add your pseudocode here
